b7566bfa49edf47a18e14668c41b8f07ad7e5fa3
unknown <leytonemail@gmail.com>
2025-08-25 21:43:09 +0200

    base class

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "linecolors" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('linecolors.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LineColors!');
-	});
-
-	context.subscriptions.push(disposable);
+  // const provider = new ColorsViewProvider
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class ColorsViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "LineColors.colorsView";
+  private _view: vscode.WebviewView;
+
+  constructor(
+    // Root of the extension for files etc
+    private readonly _extensionUri: vscode.Uri,
+  ) {}
+
+  // abstract method of WebView building & managing the webview
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+     token: vscode.CancellationToken
+    ) {
+      
+        this._view = webviewView;
+
+        webviewView.webview.options = {
+          enableScripts : true,
+          localResourceRoots : [this._extensionUri]
+        };
+        
+        // webviewView.webview.html
+
+  };
+
+};
